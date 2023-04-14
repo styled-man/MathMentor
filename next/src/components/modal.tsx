b19e453d448e3db409a2bd74ab70725819c85ead
02danieljonas <02danieljonas@gmail.com>
2023-04-14 17:34:57 -0400

    added background blue + bug fix

--- next/src/components/modal.tsx
+++ next/src/components/modal.tsx
@@ -1,23 +1,43 @@
-import React from "react";
-import { useState } from "react";
+import React from "react"
+import { useState } from "react"
 
 interface IModal {
-  userProblem: string;
+    yPosition: number
+    userProblem: string
+    infoForUser: string
 }
 
-const Modal = ({ userProblem }: IModal) => {
-  const [open, setOpen] = useState<boolean>(false);
+const Modal = ({ userProblem, yPosition, infoForUser }: IModal) => {
+    const [isModalSmall, setIsModalSmall] = useState<boolean>(true)
 
-  return (
-    <div className={`${open?"small-modal" : "large-modal"} m-5 w-[50vw] cursor-pointer p-5 shadow-lg`}>
-      You might need to revisit:{" "}
-      <span className="text-red-500 font-bold">{userProblem}</span>
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-      voluptatem quo quis nulla praesentium assumenda facere voluptate cumque
-      veritatis odit!
-    </div>
-  );
-};
+    return (
+        <>
+            {isModalSmall && (
+                <div
+                    className="fixed bottom-0 left-0 right-0 top-0 z-40 cursor-pointer backdrop-blur"
+                    onClick={() => setIsModalSmall(false)}
+                />
+            )}
 
-export default Modal;
+            <div
+                style={{
+                    top: isModalSmall ? yPosition : 0,
+                    position: isModalSmall ? "absolute" : "fixed",
+                }}
+                className={`${
+                    isModalSmall ? "small-modal" : "large-modal"
+                } cursor-pointer bg-gray-200 p-2 shadow-2xl duration-1000`}
+                onClick={() => {
+                    setIsModalSmall(!isModalSmall)
+                }}
+            >
+                You might need to revisit:{" "}
+                <span className="font-bold text-red-500">{userProblem}</span>
+                <br />
+                {infoForUser}
+            </div>
+        </>
+    )
+}
+
+export default Modal
--- next/src/pages/learn.tsx
+++ next/src/pages/learn.tsx
@@ -6,8 +6,9 @@ const learn = () => {
   
   return (
     <div>
-      <Modal userProblem={"Vector Mathematics"}/>
       learn
+      <Modal infoForUser="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, eaque laboriosam architecto facilis libero excepturi optio non, odit voluptatum impedit velit reiciendis animi ipsa asperiores obcaecati modi temporibus tempore, in laudantium aliquid ex mollitia amet iste alias. Harum ipsa enim, placeat consequuntur vero maxime odit accusamus illum saepe unde assumenda veniam corporis ducimus neque a adipisci numquam et aut libero id voluptatem facere optio asperiores? Porro modi, quam molestiae impedit incidunt quos explicabo ut. Maiores unde recusandae deleniti sit delectus, obcaecati est. Impedit voluptas cumque sequi, tempora neque molestias vero, vitae obcaecati, facere unde aliquid et omnis tempore aperiam sint!" userProblem={"Vector Mathematics"} yPosition={100} />
+      <Modal infoForUser="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, eaque laboriosam architecto facilis libero excepturi optio non, odit voluptatum impedit velit reiciendis animi ipsa asperiores obcaecati modi temporibus tempore, in laudantium aliquid ex mollitia amet iste alias. Harum ipsa enim, placeat consequuntur vero maxime odit accusamus illum saepe unde assumenda veniam corporis ducimus neque a adipisci numquam et aut libero id voluptatem facere optio asperiores? Porro modi, quam molestiae impedit incidunt quos explicabo ut. Maiores unde recusandae deleniti sit delectus, obcaecati est. Impedit voluptas cumque sequi, tempora neque molestias vero, vitae obcaecati, facere unde aliquid et omnis tempore aperiam sint!" userProblem={"Vector Mathematics"} yPosition={120} />
     </div>
   );
 };
