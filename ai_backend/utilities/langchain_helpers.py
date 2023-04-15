6a6633525ceb563ebf35568beda76b0375aaad13
Jacob Schuster <jschuster8765@gmail.com>
2023-04-15 02:42:38 -0400

    updates'

--- ai_backend/utilities/langchain_helpers.py
+++ ai_backend/utilities/langchain_helpers.py
@@ -1,6 +1,8 @@
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 # from langchain.chains import SimpleSequentialChain
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import (
+    SystemMessagePromptTemplate,
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
 )
@@ -33,3 +35,32 @@ def mathpix_2_question(chat, data: str) -> str:
 
     out = chain.run(data)
     return out
+
+
+def find_mistake(chat, data: dict) -> str:
+    response_schemas = [
+        ResponseSchema(
+            name="mistake", description="A description of the specific mistake the student made when solving the problem"),
+        ResponseSchema(
+            name="topic", description="Topic the user should review in order to correct the mistake")
+    ]
+
+    output_parser = StructuredOutputParser.from_response_schemas(
+        response_schemas)
+    format_instructions = output_parser.get_format_instructions()
+
+    system_message_prompt = SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=[],
+                                                                              template="Your job is to spot errors in problem solving and reasoning given the steps a student uses to solve a mathematics problem. You are given the steps the student attempted, as well as the correct answer. \n{format_instructions}",
+                                                        partial_variables={"format_instructions": format_instructions}))
+
+    human_message_prompt = HumanMessagePromptTemplate(prompt=PromptTemplate(
+        input_variables=["mathpix", "wolfram"],
+        template="Here is the attempt from the student: {mathpix}\n\nHere is the correct answer: {wolfram}\n\nWhat step did the student make a mistake on?",
+    ))
+
+    chat_prompt_template = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt])
+    chain = LLMChain(llm=chat, prompt=chat_prompt_template)
+
+    out = chain.run(**data)
+    return output_parser.parse(out)
