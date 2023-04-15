3aac3fa104e3a6bf71568e87eac8c7f0b7020354
Jacob Schuster <jschuster8765@gmail.com>
2023-04-15 13:32:33 -0400

    better prompts / wolfram

--- ai_backend/utilities/wolfram_helpers.py
+++ ai_backend/utilities/wolfram_helpers.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 import xml.etree.ElementTree as ET
 
@@ -12,26 +13,21 @@ def execute_wolfram_query(task: str) -> str:
     params = {
         "appid": os.getenv("WOLFRAM_ALPHA_APPID"),
         "input": task,
-        "podstate": "Result__Step-by-step solution",
+        "reinterpert": "false",
+        # "podstate": "Result__Step-by-step solution",
+        "output": "json",
         "format": "plaintext",
     }
 
     response = requests.get(URL, params=params)
     # print(response)
     response.raise_for_status()  # throw an error if the request fails
-    res = ""
 
-    # Parse the XML content of the response into an ElementTree object
-    root = ET.fromstring(response.content)
-    subpods = root.findall("./pod[@title='Results']/subpod")
+    parsed_data = json.loads(response.text)
 
-    for subpod in subpods:
-        if subpod.get("title") == "Possible intermediate steps":
-            plaintext_tag = subpod.find("plaintext")
-            if plaintext_tag is not None:
-                res = plaintext_tag.text
+    # Get desired output
+    for pod in parsed_data['queryresult']['pods']:
+        if pod['title'] == 'Result' or pod['title'] == 'Indefinite integral':
+            return pod['subpods'][0]['plaintext']
 
-    if not res:
-        raise ValueError("Wolfram Alpha did not return a result.")
-
-    return res
+    return "Unable to solve the problem"
