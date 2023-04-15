2a1faba22a7bec6481adc948237a0f06e562e931
02danieljonas <02danieljonas@gmail.com>
2023-04-15 14:57:59 -0400

    added the image urls and installed react-markdown

--- math-mentor/next.config.js
+++ math-mentor/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["picsum.photos", "images.freeimages.com"],
+},
 }
 
 module.exports = nextConfig
