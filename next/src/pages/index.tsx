d750a9f5cd059d8f803fa85677a6c9206ba2fc67
02danieljonas <02danieljonas@gmail.com>
2023-04-15 14:25:02 -0400

    landing page

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,31 +1,39 @@
-import { type NextPage } from "next";
-import DragAndDrop from "./components/DragAndDrop";
+import { type NextPage } from "next"
+import DragAndDrop from "./components/DragAndDrop"
 
 const Home: NextPage = () => {
-  const item = [
-    {
-      id: "1",
-      name: "Item 1",
-      preview: "https://picsum.photos/200/300",
-    },
-    {
-      id: "2",
-      name: "Item 2",
-      preview: "https://picsum.photos/200/300",
-    },
-    {
-      id: "3",
-      name: "Item 3",
-      preview: "https://picsum.photos/200/300",
-    },
-  ];
+    const item = [
+        {
+            id: "1",
+            name: "Item 1",
+            preview: "https://picsum.photos/200/300",
+        },
+        {
+            id: "2",
+            name: "Item 2",
+            preview: "https://picsum.photos/200/300",
+        },
+        {
+            id: "3",
+            name: "Item 3",
+            preview: "https://picsum.photos/200/300",
+        },
+    ]
 
-  return (
-    <>
-      <h1>Home</h1>
-      <DragAndDrop items={item} />
-    </>
-  );
-};
+    return (
+        <>
+            <main className="ml-7 flex h-screen flex-col justify-center">
+                <h1 className="text-9xl">
+                    <div className="flex flex-row">
+                        <span className="font-thin">Math</span>
+                        <span>{" "}</span>
+                        <span className="font-bold">Mentor</span>
+                    </div>
+                </h1>
+                <DragAndDrop items={item} />
+            </main>
+        </>
+    )
+}
 
-export default Home;
+export default Home
