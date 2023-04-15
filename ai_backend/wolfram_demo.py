15597a238e4222e744c2e6ff4f075370b5a4f6cd
Jacob Schuster <jschuster8765@gmail.com>
2023-04-14 21:10:27 -0400

    more langchain stuff

--- ai_backend/wolfram_demo.py
+++ ai_backend/wolfram_demo.py
@@ -1,11 +1,17 @@
-import os
-from langchain.utilities.wolfram_alpha import WolframAlphaAPIWrapper
-from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from dotenv import dotenv_values
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
-load_dotenv()
+api_keys = dotenv_values()
+openai_api_key = api_keys['OPENAI_API_KEY']
 
-os.environ["WOLFRAM_ALPHA_APPID"] = os.getenv("WOLFRAM_ALPHA_APPID")
-
-wolfram = WolframAlphaAPIWrapper()
-res = wolfram.run("What is 2x+5 = -3x + 7?")
-print(res)
+chat = ChatOpenAI(openai_api_key=openai_api_key,
+                  model_name='gpt-4',
+                  temperature=1)
+response = chat([SystemMessage(content='you are an expert Canadian tour guide'),
+                 HumanMessage(content='whats the most popular city in canada?')])
+print(response.content)
