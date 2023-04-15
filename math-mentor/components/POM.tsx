73f4d7481912cb677c991b095259de13632864c4
Jacob Schuster <jschuster8765@gmail.com>
2023-04-15 15:25:08 -0400

    add 3js landing page

---
+++ math-mentor/components/POM.tsx
@@ -0,0 +1,48 @@
+import * as THREE from "three"
+import React, { useRef } from "react"
+import { useGLTF } from "@react-three/drei"
+import { GLTF } from "three-stdlib"
+import { useFrame, useThree } from "react-three-fiber"
+
+type GLTFResult = GLTF & {
+    nodes: {
+        Text: THREE.Mesh
+    }
+    materials: {
+        ["Material.002"]: THREE.MeshStandardMaterial
+    }
+}
+
+export default function Model(props: JSX.IntrinsicElements["group"]) {
+    const { nodes, materials } = useGLTF("/pom.glb") as GLTFResult
+    const ref = useRef<THREE.Mesh>(null!)
+    // useThree
+    const { viewport, camera } = useThree()
+    const randZ = Math.random() * 2
+    const { width, height } = viewport.getCurrentViewport(camera, [0, 0, randZ])
+
+    useFrame((state, dt) => {
+        ref.current.position.y -= 0.0055
+
+        if (ref.current.position.y < -height - 9) {
+            ref.current.position.y = Math.random() * 13 + 11
+            ref.current.position.x = Math.random() * width - width / 2
+        }
+    })
+    return (
+        <group {...props} dispose={null}>
+            <mesh
+                scale={2}
+                ref={ref}
+                castShadow
+                receiveShadow
+                geometry={nodes.Text.geometry}
+                material={materials["Material.002"]}
+                position={[10, 31.45, -2.82]}
+                rotation={[Math.PI / 2, -0.2, 0.4]}
+            />
+        </group>
+    )
+}
+
+useGLTF.preload("/pom.glb")
