d4d5849758e2528a363cca6a6a4c89e5752ee47f
02danieljonas <02danieljonas@gmail.com>
2023-04-14 23:54:34 -0400

    made the input form its own component

--- next/src/pages/learn.tsx
+++ next/src/pages/learn.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import React, { useRef, useState } from "react"
 import Modal from "./components/Modal"
 import MessageContainer from "./components/MessageContainer"
+import Form from "./components/Form"
 
 const Learn = () => {
     return (
@@ -43,10 +44,7 @@ function InteractivityArea() {
         event.preventDefault()
         console.log(`User submitted: ${inputText}`)
         if (inputText !== "") {
-            setConversations([
-                ...conversations,
-                { message: inputText, isSentByUser: true },
-            ])
+            setConversations([...conversations, { message: inputText, isSentByUser: true }])
         }
         setInputText("")
         setIsAllModalHidden(true)
@@ -94,24 +92,12 @@ function InteractivityArea() {
                     setIsAllModalHidden={setIsAllModalHidden}
                 />
             </div>
-            <form
-                onSubmit={handleSubmit}
-                className="absolute bottom-3 right-12 flex w-[45vw] items-center  px-4 py-3"
-            >
-                <input
-                    type="text"
-                    placeholder="Type a message..."
-                    value={inputText}
-                    onChange={handleInputChange}
-                    className="mr-2 w-full resize-none rounded-md border border-gray-300 px-4 py-2"
-                />
-                <button
-                    type="submit"
-                    className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-                >
-                    Send
-                </button>
-            </form>
+
+            <Form
+                handleSubmit={handleSubmit}
+                inputText={inputText}
+                handleInputChange={handleInputChange}
+            />
         </>
     )
 }
