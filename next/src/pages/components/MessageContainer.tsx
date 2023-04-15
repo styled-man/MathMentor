07aa0f18210ef24b46b927fef835454dfb9afc0c
02danieljonas <02danieljonas@gmail.com>
2023-04-14 21:42:07 -0400

    created message components and removed learn background color

---
+++ next/src/pages/components/MessageContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessageContainer = () => {
+  return (
+    <div className='h-[100%] w-[100%] bg-blue-200'>MessageContainer</div>
+  )
+}
+
+export default MessageContainer
---
+++ next/src/pages/learn.tsx
@@ -0,0 +1,98 @@
+import Image from "next/image"
+import React, { useState } from "react"
+import Modal from "./components/Modal"
+import MessageContainer from "./components/MessageContainer"
+
+const Learn = () => {
+    return (
+        <div className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-full items-center bg-slate-100">
+            <div className="relative m-3 ml-4 h-[95vh] w-[50vw] overflow-hidden rounded-md">
+                <Image
+                    src={
+                        "https://images.freeimages.com/images/previews/54c/random-photography-3-1143357.jpg"
+                    }
+                    alt="sheesh"
+                    fill={true}
+                />
+            </div>
+            <div>
+                <InteractivityArea />
+            </div>
+        </div>
+    )
+}
+
+function InteractivityArea() {
+    const [inputText, setInputText] = useState("")
+    const [isAllModalHidden, setIsAllModalHidden] = useState<boolean>(false)
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(event.target.value)
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log(`User submitted: ${inputText}`)
+        setInputText("")
+        setIsAllModalHidden(true)
+    }
+
+    return (
+        <>
+            <div className="relative -top-[1.75rem] right-3 h-[89vh] w-[50vw]">
+                <MessageContainer />
+
+                <Modal
+                    userProblem={"Vector Mathematics"}
+                    infoForUser="Information"
+                    yPosition={100}
+                    youtubeLink="https://www.youtube.com/"
+                    youtubeLinkTitle="Math me like I'm 5"
+                    moreInfoForUser="more information"
+                    isAllModalHidden={isAllModalHidden}
+                    setIsAllModalHidden={setIsAllModalHidden}
+                />
+                <Modal
+                    userProblem={"Vector Mathematics"}
+                    infoForUser="Information"
+                    yPosition={200}
+                    youtubeLink="https://www.youtube.com/"
+                    youtubeLinkTitle="Math me like I'm 5"
+                    moreInfoForUser="more information"
+                    isAllModalHidden={isAllModalHidden}
+                    setIsAllModalHidden={setIsAllModalHidden}
+                />
+                <Modal
+                    userProblem={"Vector Mathematics"}
+                    infoForUser="Information"
+                    yPosition={300}
+                    youtubeLink="https://www.youtube.com/"
+                    youtubeLinkTitle="Math me like I'm 5"
+                    moreInfoForUser="more information"
+                    isAllModalHidden={isAllModalHidden}
+                    setIsAllModalHidden={setIsAllModalHidden}
+                />
+            </div>
+            <form
+                onSubmit={handleSubmit}
+                className="absolute bottom-3 right-12 flex w-[45vw] items-center  px-4 py-3"
+            >
+                <input
+                    type="text"
+                    placeholder="Type a message..."
+                    value={inputText}
+                    onChange={handleInputChange}
+                    className="mr-2 w-full resize-none rounded-md border border-gray-300 px-4 py-2"
+                />
+                <button
+                    type="submit"
+                    className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                >
+                    Send
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default Learn
