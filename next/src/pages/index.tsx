47f519a5a710835196d9db77967252456ca070ff
Jacob Schuster <jschuster8765@gmail.com>
2023-04-15 07:51:33 -0400

    add yt

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,31 +1,46 @@
-import { type NextPage } from "next";
-import DragAndDrop from "./components/DragAndDrop";
+import { type NextPage } from "next"
+import DragAndDrop from "./components/DragAndDrop"
+import { useEffect } from "react"
+import { api } from "~/utils/api"
+import z from "zod"
 
 const Home: NextPage = () => {
-  const item = [
-    {
-      id: "1",
-      name: "Item 1",
-      preview: "https://picsum.photos/200/300",
-    },
-    {
-      id: "2",
-      name: "Item 2",
-      preview: "https://picsum.photos/200/300",
-    },
-    {
-      id: "3",
-      name: "Item 3",
-      preview: "https://picsum.photos/200/300",
-    },
-  ];
+    const { data, error } = api.document.youtubeRecommendations.useQuery({ keywords: "college algebra" }, { onError: err => console.error(err) })
 
-  return (
-    <>
-      <h1>Home</h1>
-      <DragAndDrop items={item} />
-    </>
-  );
-};
+    if (error) {
+        // handle error
+        return <div>Error: {error.message}</div>
+    }
 
-export default Home;
+    if (!data) {
+        // data is still loading
+        return <div>Loading...</div>
+    }
+
+    const item = [
+        {
+            id: "1",
+            name: "Item 1",
+            preview: "https://picsum.photos/200/300",
+        },
+        {
+            id: "2",
+            name: "Item 2",
+            preview: "https://picsum.photos/200/300",
+        },
+        {
+            id: "3",
+            name: "Item 3",
+            preview: "https://picsum.photos/200/300",
+        },
+    ]
+
+    return (
+        <>
+            <h1>Home</h1>
+            <DragAndDrop items={item} />
+        </>
+    )
+}
+
+export default Home
