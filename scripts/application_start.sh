41c77bedb7a96e01d2f0082dc860fe01f8436117
styled <smyvensestime@gmail.com>
2023-04-15 05:16:39 -0400

    fix application start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,14 +6,6 @@ sudo chmod -R 777 /home/ec2-user/math-mentor
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/math-mentor
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
 #start our node app in the background
 docker-compose build --no-cache
 docker-compose up -d
