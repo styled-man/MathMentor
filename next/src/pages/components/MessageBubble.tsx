a2f8d9438538019b255204b988a47acc3d6616ab
02danieljonas <02danieljonas@gmail.com>
2023-04-14 23:54:57 -0400

    Gave MessageBubble support for react nodes

--- next/src/pages/components/MessageBubble.tsx
+++ next/src/pages/components/MessageBubble.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { Children, type ReactNode } from "react"
 
 interface IMessageBubble {
     isSentByUser: boolean
-    message: string
+    children: ReactNode
 }
 
-const MessageBubble = ({ isSentByUser, message }: IMessageBubble) => {
+const MessageBubble = ({ isSentByUser, children }: IMessageBubble) => {
     return (
         <div
             style={{
@@ -15,7 +15,7 @@ const MessageBubble = ({ isSentByUser, message }: IMessageBubble) => {
             }}
             className="mb-1 w-fit  max-w-[90%]  break-all rounded-2xl p-5 text-white"
         >
-            {message}
+            {children}
         </div>
     )
 }
