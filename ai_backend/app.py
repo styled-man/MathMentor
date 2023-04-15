bae7eabc085f93c6b4d7020f2827d33db06e0ced
Jacob Schuster <jschuster8765@gmail.com>
2023-04-15 01:43:33 -0400

    langchain works

--- ai_backend/app.py
+++ ai_backend/app.py
@@ -37,7 +37,14 @@ chat = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"),
                   model_name='gpt-4',
                   temperature=0.1)
 
-app = Flask(__name__)
+assemble_prompts()
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello_world():
+#    return "<p>Hello, World!</p>"
 
 
 @app.route("/", methods=['GET'])
