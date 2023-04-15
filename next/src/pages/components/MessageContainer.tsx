054401cbc30af7676754f3e67cb6cd4f99f8f4dd
02danieljonas <02danieljonas@gmail.com>
2023-04-14 23:54:57 -0400

    Gave MessageBubble support for react nodes

--- next/src/pages/components/MessageContainer.tsx
+++ next/src/pages/components/MessageContainer.tsx
@@ -13,7 +13,9 @@ const MessageContainer = ({ conversations, useRefHook }: IMessageContainer) => {
     return (
         <div className="flex h-[100%] w-[100%] flex-col overflow-auto py-5" ref={useRefHook}>
             {conversations.map(e => (
-                <MessageBubble key={e.message} message={e.message} isSentByUser={e.isSentByUser} />
+                <MessageBubble key={e.message} isSentByUser={e.isSentByUser}>
+                    {e.message}
+                </MessageBubble>
             ))}
         </div>
     )
