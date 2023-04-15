3c9162fbc4b673e1a16720fadb49326931d91bda
styled <smyvensestime@gmail.com>
2023-04-14 21:22:30 -0400

    created the document routes for trpc

--- next/src/pages/components/DragAndDrop.tsx
+++ next/src/pages/components/DragAndDrop.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-import React, { useState } from "react";
+import Image from "next/image"
+import React, { useState } from "react"
+import { api } from "~/utils/api"
 
 interface DragItem {
-  id: string;
-  preview?: string;
-  name: string;
+    id: string
+    preview?: string
+    name: string
 }
 
 interface DragAndDropProps {
@@ -16,58 +17,72 @@ const DragAndDrop: React.FC<DragAndDropProps> = ({ items, className }) => {
   const [draggedItem, setDraggedItem] = useState<DragItem | null>(null);
   const [droppedItems, setDroppedItems] = useState<DragItem[]>([]);
 
-  const handleDragStart = (
-    event: React.DragEvent<HTMLDivElement>,
-    item: DragItem
-  ) => {
-    setDraggedItem(item);
-  };
-
-  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-  };
-
-  const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    if (draggedItem) {
-      setDroppedItems([...droppedItems, draggedItem]);
-      setDraggedItem(null);
-    } else {
-      const files = Array.from(event.dataTransfer.files);
-      for (const file of files) {
-        const id = file.name;
-        const name = file.name;
-        const preview = await createPreviewImage(file); // Create preview image
-        setDroppedItems([...droppedItems, { id, name, preview }]);
-      }
+    const handleDragStart = (event: React.DragEvent<HTMLDivElement>, item: DragItem) => {
+        setDraggedItem(item)
     }
-  };
 
-  // event: React.MouseEvent<HTMLButtonElement>
-  const handleFileSelect = () => {
-    const fileInput = document.createElement("input");
-    fileInput.type = "file";
-    fileInput.onchange = async (event) => {
-      const file = (event.target as HTMLInputElement).files?.[0];
-      if (file) {
-        const id = file.name;
-        const name = file.name;
-        const preview = await createPreviewImage(file); // Create preview image
-        setDroppedItems([...droppedItems, { id, name, preview }]);
-      }
-    };
-    fileInput.click();
-  };
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault()
+    }
 
-  const createPreviewImage = (file: File): Promise<string | undefined> => {
-    return new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        resolve(reader.result as string);
-      };
-    });
-  };
+    const uploadDocument = api.document.upload.useMutation({
+        onMutate({ image, imageName }) {
+            //
+            console.log("something")
+        },
+    })
+
+    function handleDocumentUpload() {
+        droppedItems.forEach(({ name, preview }) => {
+            if (!preview) {
+                return
+            }
+            console.log(name, preview)
+            uploadDocument.mutate({ image: name, imageName: preview })
+        })
+    }
+
+    const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault()
+        if (draggedItem) {
+            setDroppedItems([...droppedItems, draggedItem])
+            setDraggedItem(null)
+        } else {
+            const files = Array.from(event.dataTransfer.files)
+            for (const file of files) {
+                const id = file.name
+                const name = file.name
+                const preview = await createPreviewImage(file) // Create preview image
+                setDroppedItems([...droppedItems, { id, name, preview }])
+            }
+        }
+    }
+
+    // event: React.MouseEvent<HTMLButtonElement>
+    const handleFileSelect = () => {
+        const fileInput = document.createElement("input")
+        fileInput.type = "file"
+        fileInput.onchange = async event => {
+            const file = (event.target as HTMLInputElement).files?.[0]
+            if (file) {
+                const id = file.name
+                const name = file.name
+                const preview = await createPreviewImage(file) // Create preview image
+                setDroppedItems([...droppedItems, { id, name, preview }])
+            }
+        }
+        fileInput.click()
+    }
+
+    const createPreviewImage = (file: File): Promise<string | undefined> => {
+        return new Promise(resolve => {
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                resolve(reader.result as string)
+            }
+        })
+    }
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -100,4 +115,4 @@ const DragAndDrop: React.FC<DragAndDropProps> = ({ items, className }) => {
   );
 };
 
-export default DragAndDrop;
+export default DragAndDrop
