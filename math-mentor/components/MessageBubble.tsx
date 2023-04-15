5c83e8751d0fc9f987d96efdb9aed20c90c64618
02danieljonas <02danieljonas@gmail.com>
2023-04-15 14:47:45 -0400

    replaced all my files from next to math-mentor

--- math-mentor/components/MessageBubble.tsx
+++ math-mentor/components/MessageBubble.tsx
@@ -1,23 +1,27 @@
-import React, { Children, type ReactNode } from "react"
-
+import React, { type ReactNode } from "react"
+import ReactMarkdown from "react-markdown"
 interface IMessageBubble {
-  isSentByUser: boolean
-  children: ReactNode
+    isSentByUser: boolean
+    children: ReactNode
+    isMessageFiller?: boolean
 }
 
-const MessageBubble = ({ isSentByUser, children }: IMessageBubble) => {
-  return (
-    <div
-      style={{
-        backgroundColor: isSentByUser ? "blue" : "#6b6b6d",
-        marginLeft: isSentByUser ? "auto" : "1.25rem",
-        marginRight: isSentByUser ? "1.25rem" : "auto",
-      }}
-      className="mb-1 w-fit  max-w-[90%]  break-all rounded-2xl p-5 text-white first:mt-[100%]"
-    >
-      {children}
-    </div>
-  )
+const MessageBubble = ({ isSentByUser, children, isMessageFiller = false }: IMessageBubble) => {
+    return (
+        <div
+            style={{
+                backgroundColor: isSentByUser ? "blue" : "#6b6b6d",
+                alignSelf: isSentByUser ? "end" : "start",
+            }}
+            className="mx-5 mb-1 w-fit  max-w-[90%] break-all rounded-2xl p-5 text-white first:mt-[100%]"
+        >
+            {isMessageFiller || isSentByUser ? (
+                children
+            ) : (
+                <ReactMarkdown>{children as string}</ReactMarkdown>
+            )}
+        </div>
+    )
 }
 
 export default MessageBubble
--- math-mentor/components/MessageContainer.tsx
+++ math-mentor/components/MessageContainer.tsx
@@ -2,23 +2,28 @@ import React, { type ReactNode } from "react"
 import MessageBubble from "./MessageBubble"
 
 interface IMessageContainer {
-  conversations: Array<{
-    message: ReactNode
-    isSentByUser: boolean
-  }>
-  useRefHook: React.RefObject<HTMLDivElement>
+    conversations: Array<{
+        message: ReactNode
+        isSentByUser: boolean
+        isMessageFiller?: boolean
+    }>
+    useRefHook: React.RefObject<HTMLDivElement>
 }
 
 const MessageContainer = ({ conversations, useRefHook }: IMessageContainer) => {
-  return (
-    <div className="flex h-[100%] w-[100%] flex-col overflow-auto py-5" ref={useRefHook}>
-      {conversations.map((e) => (
-        <MessageBubble key={e.message as string} isSentByUser={e.isSentByUser}>
-          {e.message}
-        </MessageBubble>
-      ))}
-    </div>
-  )
+    return (
+        <div className="flex h-[100%] w-[100%]  flex-col overflow-scroll py-5" ref={useRefHook}>
+            {conversations.map(e => (
+                <MessageBubble
+                    key={e.message as string}
+                    isSentByUser={e.isSentByUser}
+                    isMessageFiller={e?.isMessageFiller}
+                >
+                    {e.message}
+                </MessageBubble>
+            ))}
+        </div>
+    )
 }
 
 export default MessageContainer
